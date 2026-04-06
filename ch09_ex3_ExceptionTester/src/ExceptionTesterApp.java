c76ff0500a16c0f0b4faa09dc5667a8acc5bbe3d
Terrell Taylor <ttaylo59@smartweb.augustatech.edu>
2026-04-06 19:57:31 -0400

    commit

--- ch09_ex3_ExceptionTester/src/ExceptionTesterApp.java
+++ ch09_ex3_ExceptionTester/src/ExceptionTesterApp.java
@@ -9,18 +9,25 @@ public class ExceptionTesterApp {
     }
 
     public static void method1() {
-        System.err.println("\tIn method1: calling method2.");
-        method2();
+        try 
+        {
+            System.err.println("\tIn method1: calling method2.");
+            method2();
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("\tIn method1: could not call method2; FileNotFoundException.");
+        }
         System.err.println("\tIn method1: returned from method2.");
     }
 
-    public static void method2() {
+    public static void method2() throws FileNotFoundException {
         System.err.println("\t\tIn method2: calling method3.");
         method3();
         System.err.println("\t\tIn method2: returned from method3.");
     }
 
-    public static void method3() {
+    public static void method3() throws FileNotFoundException {
         System.err.println("\t\t\tIn method3: Entering.");
 
         //Add code to throw an exception here.
